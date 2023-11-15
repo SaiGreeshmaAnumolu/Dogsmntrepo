8735809b9b34a924c75672fa9ee1dc0c51129283
SaiGreeshmaAnumolu <123037725+SaiGreeshmaAnumolu@users.noreply.github.com>
2023-11-14 20:46:35 -0600

    first commit

---
+++ src/main/java/com/bandiClasses/DMS/repository/DogRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Dog;
+
+/**
+ * @author S558593 Sai Greeshma Anumolu
+ */
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+
+}
---
+++ src/main/java/com/bandiClasses/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.bandiClasses.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bandiClasses.DMS.Models.Trainer;
+
+/**
+ * @author S558593 Sai Greeshma Anumolu
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
